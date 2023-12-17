762da76d38f306272871cfee51f3298ac4945e35
Emanuel Escudero <escudero.mdz@gmail.com>
2023-12-17 11:34:08 -0300

    Created: LoginScreen

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+import { styles } from './HomeStyles'
+
+const Home = () => {
+  return (
+    <View style={styles.home}>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login from '../components/Login/Login'
+
+const LoginScreen = ({ navigation }) => {
+  return (
+    <Login navigation={navigation} />
+  )
+}
+
+export default LoginScreen

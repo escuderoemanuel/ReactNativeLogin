02a3add3ea5051f2b4230846bb1da22ae5cd3b9e
Emanuel Escudero <escudero.mdz@gmail.com>
2023-12-17 13:26:18 -0300

    Control Point

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 import { styles } from './HomeStyles'
 
 const Home = ({ navigation }) => {
-  
+
   return (
     <View style={styles.home}>
       <Text style={styles.homeText}>Home</Text>
 
       <View style={styles.homeButtonsContainer}>
-        <TouchableOpacity style={styles.homeButton} onPress={() => navigation.navigate('LoginScreen')}>
+        <TouchableOpacity style={styles.homeButton} onPress={() => navigation.navigate('Login')}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.homeButton} onPress={() => console.log('REG')} >
+        <TouchableOpacity style={styles.homeButton} onPress={() => navigation.navigate('Register')} >
           <Text style={styles.buttonText}>Register</Text>
         </TouchableOpacity>
       </View>
